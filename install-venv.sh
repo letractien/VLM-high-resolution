16edcba562bc58947a4c7928a69faac9679fcae4
Tiến Lê <21110677@student.hcmute.edu.vn>
2025-12-11 00:56:46 +0700

    Add file setup

---
+++ install-venv.sh
@@ -0,0 +1,15 @@
+apt update
+apt install git -y
+
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+
+source ~/miniconda3/bin/activate
+conda init --all
+
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+conda create -n .venv python=3.11.9 -y
+conda activate .venv
