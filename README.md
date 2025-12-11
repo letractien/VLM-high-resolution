b4ec18a73e3d09dc2feab2f0e292661125570f7c
Tiến Lê <ww.hacker01@gmail.com>
2025-12-11 08:53:52 +0000

    Add Google Drive upload scripts and update README for library installations

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ conda create --name .venv-idefics2 --clone .venv
 conda activate .venv-idefics2
 
 ## Install torch + other library
+pip install bitsandbytes
 pip install numpy==1.26.4
 pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
 pip install flash-attn===1.0.4 --no-build-isolation
---
+++ push_cache.py
@@ -0,0 +1,38 @@
+import argparse
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+import tarfile
+import os
+
+def auth_drive():
+    """OAuth2 and return GoogleDrive"""
+    gauth = GoogleAuth()
+    gauth.LoadClientConfigFile("./client_secrets.json")
+    # gauth.LocalWebserverAuth()
+    gauth.CommandLineAuth()  
+    return GoogleDrive(gauth)
+
+def compress_folder(folder_path, output_filename):
+    """Zip folder to tar.gz"""
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(folder_path, arcname=os.path.basename(folder_path))
+    print(f"📦 Zipped folder {folder_path} to {output_filename}")
+
+def upload_file(drive, file_path, parent_id):
+    """Upload file to Google Drive to folder have parent_id"""
+    file_name = os.path.basename(file_path)
+    gfile = drive.CreateFile({'title': file_name, 'parents': [{'id': parent_id}]})
+    gfile.SetContentFile(file_path)
+    gfile.Upload()
+    print(f"✅ Uploaded {file_name} to Drive folder ID {parent_id}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upload folder to Google Drive as a tar.gz file.")
+    parser.add_argument("--folder", type=str, default="cache")
+    parser.add_argument("--tarfile", type=str, default="cache.tar.gz")
+    parser.add_argument("--drive_folder_id", type=str, default="1KTomPYcsQiBPrRj7bjHrvT78XBN3dbCC")
+    args = parser.parse_args()
+
+    drive = auth_drive()
+    compress_folder(args.folder, args.tarfile)
+    upload_file(drive, args.tarfile, args.drive_folder_id)
